aaf3ad150ecb7e9a0715ca815b7eebbeed59db85
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2022-02-07 12:16:36 -0800

    Update For_loop.py

--- For_loop.py
+++ For_loop.py
@@ -1,3 +1,30 @@
-for x in range(51):
+result = 0
+low = int(input("Input starting number: "))
+high = int(input("Input ending number: "))
+mult = int(input("Input the multiple number: "))
+
+
+for x in range(151):
     print(x)
 
+for x in range(5, 1001, 5):
+    print(x)
+
+for x in range(1, 100):
+    if x % 10 == 0:
+        print('coding dojo')
+    elif x % 5 == 0:
+        print('coding')
+    else:
+        print(x)
+
+for x in range(500001):
+    if x % 2 != 0:
+        result += x
+print (result)
+
+for x in range(2018, 0, -4):
+    print(x)
+
+for x in range(low, high, mult):
+    print(x)
